2ff9439082202c45b3867fa3d80efaf8e61d150a
Francesco Tassi <tassi.francesco@gmail.com>
2023-09-11 09:03:21 +0200

    Implement health_check api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpServer, Responder, HttpResponse};
+
+#[get("/health_check")]
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    HttpServer::new(|| {
+        App::new()
+            .service(health_check)
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
