2ef584fbdbb42bc5f6b2ab732b3fe644ea88383e
Francesco Tassi <tassi.francesco@gmail.com>
2023-09-13 10:31:23 +0200

    Extract code to a routes module

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,8 @@
-use actix_web::{dev::Server, get, post, web, App, HttpResponse, HttpServer, Responder};
+use crate::routes::*;
+use actix_web::{dev::Server, App, HttpServer};
 use std::net::TcpListener;
 
-#[get("/health_check")]
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
-
-#[derive(serde::Deserialize)]
-struct FormData {
-    email: String,
-    name: String,
-}
-
-#[post("/subscriptions")]
-async fn subscriptions(_form: web::Form<FormData>) -> impl Responder {
-    HttpResponse::Ok()
-}
+mod routes;
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| App::new().service(health_check).service(subscriptions))
