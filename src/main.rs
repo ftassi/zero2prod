d04e3252588a7c17c05ab720677af6d0a5b5cd98
Francesco Tassi <tassi.francesco@gmail.com>
2023-09-13 11:17:46 +0200

    Extract some param to a config file
    Now sure about the config file itself though. I think env variables
    would have a better approach, just following the book for now

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = std::net::TcpListener::bind("127.0.0.1:8000")?;
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let listener =
+        std::net::TcpListener::bind(format!("127.0.0.1:{}", configuration.application_port))?;
     run(listener)?.await
 }
