63fd21e5b606d572231cbabc1fcfe2ef2b61511f
Francesco Tassi <tassi.francesco@gmail.com>
2023-09-15 23:11:06 +0200

    Split integration tests by route
    All the setup related code is still duplicated. I will remove such
    duplication later on.

--- tests/subscriptions.rs
+++ tests/subscriptions.rs
@@ -6,17 +6,56 @@ use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use reqwest::StatusCode;
 
 #[tokio::test]
-async fn health_check_should_return_ok() {
+async fn subscribe_returns_a_200_for_valid_form_data() {
     let app_data = spawn_app().await;
+    let _configuration = get_configuration().expect("Failed to read configuration.");
+
     let client = reqwest::Client::new();
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
     let response = client
-        .get(format!("{}/health_check", &app_data.address))
+        .post(&format!("{}/subscriptions", &app_data.address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
         .send()
         .await
-        .expect("Failed to execute request.");
+        .expect("Failed to execut request");
 
-    assert_eq!(response.status(), StatusCode::OK);
-    assert_eq!(Some(0), response.content_length());
+    assert_eq!(200, response.status().as_u16());
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&app_data.connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let app_data = spawn_app().await;
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%guin", "missing the email"),
+        ("email=ursula_le_guid%40gmail.com", "missing the name"),
+        ("", "missing both"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app_data.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execut request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
 }
 
 async fn configure_database(config: &DatabaseSettings) -> PgPool {
