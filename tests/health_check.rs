0bc9a8647dfd59d78a32d5d14af69358bd7b0d15
Francesco Tassi <tassi.francesco@gmail.com>
2023-09-16 19:14:18 +0200

    Clippy

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -97,7 +97,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
 
 async fn configure_database(config: &DatabaseSettings) -> PgPool {
     let mut connection =
-        PgConnection::connect(&config.connection_string_without_db().expose_secret())
+        PgConnection::connect(config.connection_string_without_db().expose_secret())
             .await
             .expect("Failed to connect to Postgres.");
 
@@ -106,7 +106,7 @@ async fn configure_database(config: &DatabaseSettings) -> PgPool {
         .await
         .expect("Failed to create database.");
 
-    let connection_pool = PgPool::connect(&config.connection_string().expose_secret())
+    let connection_pool = PgPool::connect(config.connection_string().expose_secret())
         .await
         .expect("Failed to connect to Postgres.");
 
