6f62cee5982f2a89d7b3c02f0683874ceca85edf
Francesco Tassi <tassi.francesco@gmail.com>
2023-09-16 10:40:38 +0200

    Revert "Split integration tests by route"
    I do not want to deal with the duplication yet, keeping it simple for
    now.
    This reverts commit 63fd21e5b606d572231cbabc1fcfe2ef2b61511f.

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -19,6 +19,59 @@ async fn health_check_should_return_ok() {
     assert_eq!(Some(0), response.content_length());
 }
 
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let app_data = spawn_app().await;
+    let _configuration = get_configuration().expect("Failed to read configuration.");
+
+    let client = reqwest::Client::new();
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &app_data.address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execut request");
+
+    assert_eq!(200, response.status().as_u16());
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&app_data.connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let app_data = spawn_app().await;
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%guin", "missing the email"),
+        ("email=ursula_le_guid%40gmail.com", "missing the name"),
+        ("", "missing both"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app_data.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execut request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
+}
+
 async fn configure_database(config: &DatabaseSettings) -> PgPool {
     let connection_string = config.connection_string();
     let mut connection = PgConnection::connect(&config.connection_string_without_db())
