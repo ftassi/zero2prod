c45e61bdf188362cd8bafb40949dabffb0cccf7a
Francesco Tassi <tassi.francesco@gmail.com>
2023-09-17 23:48:05 +0200

    Allow booting without connecting to a DB
    Also acquiring timeout has been set to 2 seconds to have a fast fail in
    case the DB is not reachable

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use secrecy::ExposeSecret;
+use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -12,10 +13,10 @@ async fn main() -> Result<(), std::io::Error> {
         std::io::stdout,
     ));
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool =
-        PgPool::connect(configuration.database.connection_string().expose_secret())
-            .await
-            .expect("Failed to connect to Postgres.");
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(configuration.database.connection_string().expose_secret())
+        .expect("Failed to connect to Postgres.");
 
     let listener = std::net::TcpListener::bind(format!(
         "{}:{}",
