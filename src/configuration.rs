9472c7306a60ac60933237f90ddf050201393592
Francesco Tassi <tassi.francesco@gmail.com>
2023-09-20 22:41:40 +0200

    Deserialize number for string config params
    ENV variables are strings for the config crate, so we need to specify a
    custom deserialization strategy to be used rather than the default one.

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,4 +1,5 @@
 use secrecy::{ExposeSecret, Secret};
+use serde_aux::field_attributes::deserialize_number_from_string;
 
 pub enum Environment {
     Local,
@@ -40,12 +41,14 @@ pub struct DatabaseSettings {
     pub username: String,
     pub password: Secret<String>,
     pub host: String,
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub database_name: String,
 }
 
 #[derive(serde::Deserialize)]
 pub struct ApplicationSettings {
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
 }
