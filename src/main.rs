96dbf1df08fea3613065c1116bbebf0cea4f3bae
Francesco Tassi <tassi.francesco@gmail.com>
2023-09-21 00:07:59 +0200

    Raise pool timeout
    Maybe 2 secs is a bit too low for this DigitalOcean setup?
    I am not sure this will work...

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ async fn main() -> Result<(), std::io::Error> {
     ));
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPoolOptions::new()
-        .acquire_timeout(std::time::Duration::from_secs(2))
+        .acquire_timeout(std::time::Duration::from_secs(15))
         .connect_lazy_with(configuration.database.with_db());
 
     let listener = std::net::TcpListener::bind(format!(
